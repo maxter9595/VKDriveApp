91677bd4c9006699a7467ff3601257c00251c487
Maxim Terletskii <max.t95@bk.ru>
2025-09-15 21:21:21 +0300

    Modified instruction

--- backend/sql/init-users.sql
+++ backend/sql/init-users.sql
@@ -1,7 +1,7 @@
 INSERT IGNORE INTO users (email, password, first_name, last_name, role, is_active, created_at, updated_at)
 SELECT 
     'admin@vkdrive.local',
-    '$2a$12$T/Nto9IzdLrg./VBKGH8e.eO.XPHh5F9SR2VNtjnag9BZ0.3p0Pzi',
+    '$2a$12$b28xwKoN0xV.cakuej5jJus64S5ZoAY8a3U8Tu5FjHRjj5PVFEBUe',
     'System',
     'Administrator',
     'admin',
@@ -14,7 +14,7 @@ WHERE NOT EXISTS (SELECT 1 FROM users WHERE email = 'admin@vkdrive.local' LIMIT 
 INSERT IGNORE INTO users (email, password, first_name, last_name, role, is_active, created_at, updated_at)
 SELECT 
     'test.user@vkdrive.local',
-    '$2a$12$5QsSYwP8Qvg14n.LCrYnxOJJLUc7RVc7Dow6ozuzxrYEOl0Oct0KG',
+    '$2a$12$AuckOEbpd2RCXYGJ.Mf5huDOng0uVeJnFe229fpzYb1SOLlUgSxr2',
     'Test',
     'User',
     'user',
